72fa7afbe0b4928d7231ce799d263910ea752b9a
TheSalmanK <115428798+TheSalmanK@users.noreply.github.com>
2023-04-20 00:32:49 -0400

    gjh
    hjhb

--- dynamtionary.js
+++ dynamtionary.js
@@ -1,1 +1,32 @@
-console.log('did somebody invite dynamtionary??')
+console.log("did someone invite dynamtionary to the party?");
+
+const theDiv = document.querySelector('div');
+const textArea = document.querySelector("textarea");
+textArea.addEventListener("input", (inputEvent) => {
+  const tokens = textArea.value.split(" ");
+  const spans = tokens.map(str2Span);
+
+  theDiv.innerHTML = '';
+  theDiv.append(...spans);
+});
+
+function str2Span(str) {
+  const span = document.createElement('span');
+  span.innerText = str;
+  if (knownWords.has(str)) {
+    span.classList.add('highlight');
+  }
+  span.innerHTML += ' '; // Add space between words
+  return span;
+}
+
+const knownWords = new Set();
+
+const learnElem = document.getElementById('learn');
+learnElem.addEventListener('keyup', (ev) => {
+  if (ev.key === 'Enter') {
+    knownWords.add(learnElem.value);
+    learnElem.value = '';
+    console.table(knownWords);
+  }
+});
